e5b87c8221a17f8da8d2afa3777c346b18e04145
Daffa fauzan <daffafauzand24@gmail.com>
2022-12-25 01:57:47 +0700

    fist commit

---
+++ travel_website/package.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>package</title>
+
+    <!-- swiper css link  -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+    <!-- header section starts  -->
+
+    <section class="header">
+        <a href="home.php" class="logo">Pariwisata Jawa Timur</a>
+
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="package.php">package</a>
+            <a href="book.php">book</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+    </section>
+
+    <!-- header section ends -->
+
+    <div class="heading" style="background: url(images/header-bg-2.png) no-repeat">
+        <h1>packages</h1>
+    </div>
+
+    <!-- packages section starts  -->
+
+    <section class="packages">
+        <h1 class="heading-title">top destinasi</h1>
+
+        <div class="box-container">
+            <div class="box">
+                <div class="image">
+                    <img src="images/medium_semeru-pronojiwo.jpg" alt="" />
+                </div>
+                <div class="content">
+                    <h3>Gunung Semeru</h3>
+                    <p>
+                        Terpilih sebagai gunung tertinggi di Pulau Jawa, Gunung Semeru terkenal dengan keindahannya yang
+                        mampu memanjakan mata para pendaki.
+                    </p>
+                    <a href="book.php" class="btn">book now</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/Pantai-Papuma-Jember.jpg" alt="" />
+                </div>
+                <div class="content">
+                    <h3>Pantai Papuma Jember</h3>
+                    <p>
+                        Keindahan yang dimilikinya bahkan disandingkan dengan Raja Ampat, Papua. Karena di sepanjang
+                        pantainya terdapat bebatuan karang dengan ukuran yang beragam.
+                    </p>
+                    <a href="book.php" class="btn">book now</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/Wisata-Bahari-Lamongan.jpg" alt="" />
+                </div>
+                <div class="content">
+                    <h3>Wisata Bahari Lamongan</h3>
+                    <p>
+                        Taman bermain yang dibangun di Tanjung Kodok ini menjadi tempat menghabiskan akhir pekan dengan
+                        aneka wahana bermain yang menarik, karena berada di dekat pantai. Wisatawan dapat bermain di
+                        kolam dengan aneka seluncuran.
+                    </p>
+                    <a href="book.php" class="btn">book now</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/29-malang-night-paradise.jpg" alt="" />
+                </div>
+                <div class="content">
+                    <h3>Malang Night Paradise</h3>
+                    <p>
+                        Malang Night Paradise merupakan wisata malam terbesar di Jawa Timur yang menawarkan keindahan
+                        gemerlap taman lampu LED dan taman lampion, dan merupakan satu-satunya tempat foto dengan
+                        standar internasional.
+                    </p>
+                    <a href="book.php" class="btn">book now</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/010873500_1567478338-kawah_ijen.jpeg" alt="" />
+                </div>
+                <div class="content">
+                    <h3>Kawah Ijen</h3>
+                    <p>
+                        Terletak di kawasan Banyuwangi, kawah dari gunung yang terus mengeluarkan belerang ini menjadi
+                        tujuan wisatawan yang ingin merasakan pendakian kecil sembari menyaksikan bentang alam yang
+                        menakjubkan.
+                    </p>
+                    <a href="book.php" class="btn">book now</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/img-13.jpeg" alt="" />
+                </div>
+                <div class="content">
+                    <h3>Pantai Klayar</h3>
+                    <p>
+                        Pantai Klayar yang menawarkan deburan air ke udara. Saat ombak mengenai batu karang, buliran air
+                        akan terciprat ke urada sehingga siapa saja yang berapa di dekat lokasi bisa basah kuyup tanpa
+                        harus mencebur ke laut.
+                    </p>
+                    <a href="book.php" class="btn">book now</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/img-14.jpg" alt="" />
+                </div>
+                <div class="content">
+                    <h3>Gili Labak</h3>
+                    <p>
+                        Gili Labak yang keindahannya bisa menyamai Gili Trawangan yang ada di kawasan Lombok, Nusa
+                        Tenggara Timur. Wisatawan bisa menyaksikan birunya lautan yang bening dan juga pulau kecil yang
+                        memiliki pasir putih nan lembut.
+                    </p>
+                    <a href="book.php" class="btn">book now</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/img-15.jpg" alt="" />
+                </div>
+                <div class="content">
+                    <h3>Taman Nasional Baluran</h3>
+                    <p>
+                        atraksi paling menarik di Taman Nasional Baluran ini ada padang sabana yang sangat luas. saat
+                        musim hujan tiba, kawasan ini akan menjadi hijau dan dipenuhi banyak sekali binatan liar.
+                    </p>
+                    <a href="book.php" class="btn">book now</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/img-16.jpg" alt="" />
+                </div>
+                <div class="content">
+                    <h3>Air Terjun Coban Rondo</h3>
+                    <p>
+                        Air Terjun Coban Rondo adalah destinasi unggulan di Batu selain aneka taman bermain.
+                        Terletak di lereng pengunungan.
+                    </p>
+                    <a href="book.php" class="btn">book now</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/img-11.jpg" alt="" />
+                </div>
+                <div class="content">
+                    <h3>adventure & tour</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!
+                    </p>
+                    <a href="book.php" class="btn">book now</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/img-12.jpg" alt="" />
+                </div>
+                <div class="content">
+                    <h3>adventure & tour</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!
+                    </p>
+                    <a href="book.php" class="btn">book now</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="load-more"><span class="btn">load more</span></div>
+    </section>
+
+    <!-- packages section ends -->
+
+    <!-- footer section starts  -->
+
+    <section class="footer">
+        <div class="box-container">
+            <div class="box">
+                <h3>quick links</h3>
+                <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+                <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+                <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+                <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+            </div>
+
+            <div class="box">
+                <h3>extra links</h3>
+                <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+            </div>
+
+            <div class="box">
+                <h3>contact info</h3>
+                <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+                <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+                <a href="#"> <i class="fas fa-envelope"></i> shaikhanas@gmail.com </a>
+                <a href="#"> <i class="fas fa-map"></i> mumbai, india - 400104 </a>
+            </div>
+
+            <div class="box">
+                <h3>follow us</h3>
+                <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+                <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+                <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+                <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+            </div>
+        </div>
+
+        <div class="credit">
+            created by <span>mr. web designer</span> | all rights reserved!
+        </div>
+    </section>
+
+    <!-- footer section ends -->
+
+    <!-- swiper js link  -->
+    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+    <!-- custom js file link  -->
+    <script src="js/script.js"></script>
+</body>
+
+</html>
